05d9635a4f348a42f0664224b7b35ccc073deb6c
alexandrerazo <alexandre.otrente@etec.sp.gov.br>
2025-02-25 18:19:32 -0300

    funçoes node

---
+++ aula2/index.js
@@ -0,0 +1,16 @@
+function soma (n1,n2){
+    return (n1+n2)
+}
+
+console.log (soma (13,63))
+
+function soma (n1,n2){
+    return (n1-n2)
+}
+
+
+function soma (n1,n2){
+    return (n1*n2)}
+    
+function soma (n1,n2){
+    return (n1/n2)}
